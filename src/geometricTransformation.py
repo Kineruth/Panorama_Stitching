ffd95f2bd245c98218fcf2d7ae5019a773284c39
DoriyaSpielman <doriyaspielman@gmail.com>
2019-10-03 14:42:41 +0300

    function findFeatures
    find key points and match them.
    return matrix of match coordinates.

--- src/geometricTransformation.py
+++ src/geometricTransformation.py
@@ -13,4 +13,46 @@ import numpy as np
 import argparse
 
 def findFeatures(img1, img2):
+    # -- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    detector = cv.xfeatures2d.SIFT_create(0, 3, 0)
+
+    # detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
+    keypoints1, descriptors1 = detector.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = detector.detectAndCompute(img2, None)
+
+    # -- Step 2: Matching descriptor vectors with a FLANN based matcher
+    # Since SURF is a floating-point descriptor NORM_L2 is used
+    matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
+    knn_matches = matcher.knnMatch(descriptors1, descriptors2, 2)
+
+    # -- Filter matches using the Lowe's ratio test
+    ratio_thresh = 0.5
+    good_matches = []
+    good_matches2 = []
+    list_kp1 = []
+    list_kp2 = []
+    for m, n in knn_matches:
+        if m.distance < ratio_thresh * n.distance:
+            good_matches.append(m)
+            good_matches2.append(n)
+            img1_idx = m.queryIdx
+            img2_idx = n.trainIdx
+            (x1, y1) = keypoints1[img1_idx].pt
+            (x2, y2) = keypoints2[img2_idx].pt
+            list_kp1.append((x1, y1))
+            list_kp2.append((x2, y2))
+
+    ans = [list_kp1, list_kp2]
+
+    # print the points (x,y) for each picture.
+    # print(ans)
+
+    # -- Draw matches
+    img_matches = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+    cv.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches,
+                   flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # -- Show detected matches
+
+    cv.imshow('Good Matches', img_matches)
+    cv.waitKey()
 
