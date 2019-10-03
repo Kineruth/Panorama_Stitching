13dee86d7189730157a8740ed0fbb513decf2ef1
DoriyaSpielman <doriyaspielman@gmail.com>
2019-10-03 17:49:24 +0300

    ransac & homography

--- src/geometricTransformation.py
+++ src/geometricTransformation.py
@@ -10,6 +10,8 @@
 from __future__ import print_function
 import cv2 as cv
 import numpy as np
+import random
+
 import argparse
 
 def findMatchFeatures(img1, img2):
@@ -43,5 +45,71 @@ def findMatchFeatures(img1, img2):
     #  print(corrList)
     return corrList
 
+def applyHomography(randomFour):
+    # loop through correspondences and create assemble matrix
+    aList = []
+    for r in randomFour:
+        p1 = np.ndarray([r.item(0), r.item(1), 1]) #2 pairs of matched points
+        p2 = np.ndarray([r.item(2), r.item(3), 1]) #2 pairs of matched points
+#********************************
+        a2 = [0, 0, 0, -p2.item(2) * p1.item(0), -p2.item(2) * p1.item(1), -p2.item(2) * p1.item(2),
+              p2.item(1) * p1.item(0), p2.item(1) * p1.item(1), p2.item(1) * p1.item(2)]
+        a1 = [-p2.item(2) * p1.item(0), -p2.item(2) * p1.item(1), -p2.item(2) * p1.item(2), 0, 0, 0,
+              p2.item(0) * p1.item(0), p2.item(0) * p1.item(1), p2.item(0) * p1.item(2)]
+        aList.append(a1)
+        aList.append(a2)
+
+    matrixA = np.ndarray(aList)
+
+    # svd composition
+    u, s, v = np.linalg.svd(matrixA)
+
+    # reshape the min singular value into a 3 by 3 matrix
+    h = np.reshape(v[8], (3, 3))
+
+    # normalize and now we have h
+    h = (1 / h.item(8)) * h
+    return h
 
 
+def geometricDistance(correspondence, h):
+        p1 = np.transpose(np.matrix([correspondence[0].item(0), correspondence[0].item(1), 1]))
+        estimatep2 = np.dot(h, p1)
+        estimatep2 = (1 / estimatep2.item(2)) * estimatep2
+
+        p2 = np.transpose(np.matrix([correspondence[0].item(2), correspondence[0].item(3), 1]))
+        error = p2 - estimatep2
+        return np.linalg.norm(error)
+
+
+def ransacHomography(corr, threshold):
+        maxInliers = []
+        finalH = None
+        for i in range(1000):
+            # find 4 random points to calculate a homography
+            corr1 = corr[random.randrange(0, len(corr))]
+            corr2 = corr[random.randrange(0, len(corr))]
+            randomFour = np.vstack((corr1, corr2))
+            corr3 = corr[random.randrange(0, len(corr))]
+            randomFour = np.vstack((randomFour, corr3))
+            corr4 = corr[random.randrange(0, len(corr))]
+            randomFour = np.vstack((randomFour, corr4))
+
+            # call the homography function on those points
+            h = applyHomography(randomFour)
+            inliers = []
+
+            for i in range(len(corr)):
+                d = geometricDistance(corr[i], h)
+                if d < 5:
+                    inliers.append(corr[i])
+
+            if len(inliers) > len(maxInliers):
+                maxInliers = inliers
+                finalH = h
+            print
+            "Corr size: ", len(corr), " NumInliers: ", len(inliers), "Max inliers: ", len(maxInliers)
+
+            if len(maxInliers) > (len(corr) * threshold):
+                break
+                return finalH, maxInliers
